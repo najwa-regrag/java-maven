b63ed746c354a59b121a05a7c65555634aecc637
regrag <najwaregrag592@gmail.com>
2026-05-02 00:57:18 +0200

    test pipeline

--- maven/src/main/java/maven/Hello.java
+++ maven/src/main/java/maven/Hello.java
@@ -4,7 +4,8 @@ public class Hello {
 	
 	public static void main(String[] args)
     {
-        System.out.println("Hello Jenkins, Today is Monday, 13 Avril 2026, V1");
+        System.out.println("Hello CI, Today is Monday, 13 Avril 2026, V1");
+    
     }
 
 }
