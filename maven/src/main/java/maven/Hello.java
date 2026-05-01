545048bf4a89a37790946fb3c74342f85bd70168
regrag <najwaregrag592@gmail.com>
2026-05-02 00:47:27 +0200

    update class hello

--- maven/src/main/java/maven/Hello.java
+++ maven/src/main/java/maven/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 	
 	public static void main(String[] args)
     {
-        System.out.println("Hello DevOps, Today is Monday, 13 Avril 2026, V1");
+        System.out.println("Hello Jenkins, Today is Monday, 13 Avril 2026, V1");
     }
 
 }
