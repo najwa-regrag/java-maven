f48269c2a271f813292e5dff99aff38e6550711b
najwa-regrag <najwaregrag592@gmail.com>
2026-05-02 12:17:57 +0200

    Update Hello.java

--- maven/src/main/java/maven/Hello.java
+++ maven/src/main/java/maven/Hello.java
@@ -5,7 +5,8 @@ public class Hello {
 	public static void main(String[] args)
     {
         System.out.println("Hello jenkins, Today is Monday, 13 Avril 2026, V1");
-    
+		System.out.println("Hello pipeline, Today is Monday, 13 Avril 2026, V1");
+        
     }
 
 }
